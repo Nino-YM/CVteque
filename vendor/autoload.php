39722f96dfc554316fb2047e8b1c2b672b2ee262
Yanis Morin <yanis.morin@ecoles-epsi.net>
2024-11-26 13:42:44 +0100

    changement de l'architecture

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -22,4 +22,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit9e1b8ae2d68fe6a5f8a1dad41fb33249::getLoader();
+return ComposerAutoloaderInit626b9e7ddd47fb7eff9aaa53cce0c9ad::getLoader();
