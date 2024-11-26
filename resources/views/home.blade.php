72fd8da0a460d863fd8b7379a0313316cdd89735
Yanis Morin <yanis.morin@ecoles-epsi.net>
2024-11-26 15:46:18 +0100

    modif page d'accueil

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,27 +2,6 @@
 
 @section('content')
 <div class="container">
-    <h1 class="text-center my-5">Welcome to the Resume Viewer</h1>
-
-    <p class="text-center">
-        Browse resumes by specialization, search for students, or upload a resume.
-    </p>
-
-
-    <!-- List of Specializations -->
-    <h2 class="text-center mb-4">Specializations</h2>
-    <div class="row">
-        @foreach ($specializations as $specialization)
-        <div class="col-md-4 mb-4">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">{{ $specialization->name }}</h5>
-                    <p class="card-text">{{ $specialization->description }}</p>
-                    <a href="{{ route('specializations.show', $specialization->id) }}" class="btn btn-primary">View Resumes</a>
-                </div>
-            </div>
-        </div>
-        @endforeach
-    </div>
+    <h1 class="text-center my-5">Bienvenue dans la CVthèque de l'EPSI</h1>
 </div>
 @endsection
